fa4082a8420f6c2c2fef9c682e107a3edf1aec64
ptdev <gomes.roberto@gmail.com>
2014-03-06 04:25:23 +0000

    Changed error detection a bit so that the interface still gets visible and allow changing to another server.

--- includes/common.inc.php
+++ includes/common.inc.php
@@ -84,6 +84,11 @@ if (!isset($server['seperator'])) {
 
 // Setup a connection to Redis.
 $redis = !$server['port'] ? new Predis\Client($server['host']) : new Predis\Client('tcp://'.$server['host'].':'.$server['port']);
+try {
+    $redis->connect();
+} catch (Predis\CommunicationException $exception) {
+    $redis = false;
+}
 
 if (isset($server['auth'])) {
   if (!$redis->auth($server['auth'])) {
--- index.php
+++ index.php
@@ -2,48 +2,125 @@
 
 require_once 'includes/common.inc.php';
 
+if($redis) {
 
-try {
-  // Get keys from Redis according to server-config.
-  $keys = $redis->keys($server['filter']);
-} catch (Exception $e) {
-  die('ERROR: Can\'t connect to Redis');
-}
+    $keys = $redis->keys($server['filter']);
 
-sort($keys);
+    sort($keys);
 
-$namespaces = array(); // Array to hold our top namespaces.
+    $namespaces = array(); // Array to hold our top namespaces.
 
-// Build an array of nested arrays containing all our namespaces and containing keys.
-foreach ($keys as $key) {
-  // Ignore keys that are to long (Redis supports keys that can be way to long to put in an url).
-  if (strlen($key) > $config['maxkeylen']) {
-    continue;
-  }
+    // Build an array of nested arrays containing all our namespaces and containing keys.
+    foreach ($keys as $key) {
+      // Ignore keys that are to long (Redis supports keys that can be way to long to put in an url).
+      if (strlen($key) > $config['maxkeylen']) {
+        continue;
+      }
 
-  $key = explode($server['seperator'], $key);
+      $key = explode($server['seperator'], $key);
 
-  // $d will be a reference to the current namespace.
-  $d = &$namespaces;
+      // $d will be a reference to the current namespace.
+      $d = &$namespaces;
 
-  // We loop though all the namespaces for this key creating the array for each.
-  // Each time updating $d to be a reference to the last namespace so we can create the next one in it.
-  for ($i = 0; $i < (count($key) - 1); ++$i) {
-    if (!isset($d[$key[$i]])) {
-      $d[$key[$i]] = array();
+      // We loop though all the namespaces for this key creating the array for each.
+      // Each time updating $d to be a reference to the last namespace so we can create the next one in it.
+      for ($i = 0; $i < (count($key) - 1); ++$i) {
+        if (!isset($d[$key[$i]])) {
+          $d[$key[$i]] = array();
+        }
+
+        $d = &$d[$key[$i]];
+      }
+
+      // Nodes containing an item named __phpredisadmin__ are also a key, not just a directory.
+      // This means that creating an actual key named __phpredisadmin__ will make this bug.
+      $d[$key[count($key) - 1]] = array('__phpredisadmin__' => true);
+
+      // Unset $d so we don't accidentally overwrite it somewhere else.
+      unset($d);
     }
 
-    $d = &$d[$key[$i]];
-  }
+    // Recursive function used to print the namespaces.
+    function print_namespace($item, $name, $fullkey, $islast) {
+      global $config, $server, $redis;
 
-  // Nodes containing an item named __phpredisadmin__ are also a key, not just a directory.
-  // This means that creating an actual key named __phpredisadmin__ will make this bug.
-  $d[$key[count($key) - 1]] = array('__phpredisadmin__' => true);
+      // Is this also a key and not just a namespace?
+      if (isset($item['__phpredisadmin__'])) {
+        // Unset it so we won't loop over it when printing this namespace.
+        unset($item['__phpredisadmin__']);
 
-  // Unset $d so we don't accidentally overwrite it somewhere else.
-  unset($d);
-}
+        $type  = $redis->type($fullkey);
+        $class = array();
+        $len   = false;
 
+        if (isset($_GET['key']) && ($fullkey == $_GET['key'])) {
+          $class[] = 'current';
+        }
+        if ($islast) {
+          $class[] = 'last';
+        }
+
+        // Get the number of items in the key.
+        if (!isset($config['faster']) || !$config['faster']) {
+          switch ($type) {
+            case 'hash':
+              $len = $redis->hLen($fullkey);
+              break;
+
+            case 'list':
+              $len = $redis->lLen($fullkey);
+              break;
+
+            case 'set':
+              // This is currently the only way to do this, this can be slow since we need to retrieve all keys
+              $len = count($redis->sMembers($fullkey));
+              break;
+
+            case 'zset':
+              // This is currently the only way to do this, this can be slow since we need to retrieve all keys
+              $len = count($redis->zRange($fullkey, 0, -1));
+              break;
+          }
+        }
+
+
+        ?>
+        <li<?php echo empty($class) ? '' : ' class="'.implode(' ', $class).'"'?>>
+        <a href="?view&amp;s=<?php echo $server['id']?>&amp;key=<?php echo urlencode($fullkey)?>"><?php echo format_html($name)?><?php if ($len !== false) { ?><span class="info">(<?php echo $len?>)</span><?php } ?></a>
+        </li>
+        <?php
+      }
+
+      // Does this namespace also contain subkeys?
+      if (count($item) > 0) {
+        ?>
+        <li class="folder<?php echo ($fullkey === '') ? '' : ' collapsed'?><?php echo $islast ? ' last' : ''?>">
+        <div class="icon"><?php echo format_html($name)?>&nbsp;<span class="info">(<?php echo count($item)?>)</span>
+        <?php if ($fullkey !== '') { ?><a href="delete.php?s=<?php echo $server['id']?>&amp;tree=<?php echo urlencode($fullkey)?>:" class="deltree"><img src="images/delete.png" width="10" height="10" title="Delete tree" alt="[X]"></a><?php } ?>
+        </div><ul>
+        <?php
+
+        $l = count($item);
+
+        foreach ($item as $childname => $childitem) {
+          // $fullkey will be empty on the first call.
+          if ($fullkey === '') {
+            $childfullkey = $childname;
+          } else {
+            $childfullkey = $fullkey.$server['seperator'].$childname;
+          }
+
+          print_namespace($childitem, $childname, $childfullkey, (--$l == 0));
+        }
+
+        ?>
+        </ul>
+        </li>
+        <?php
+      }
+    }
+
+}  // if redis
 
 
 // This is basically the same as the click code in index.js.
@@ -59,92 +136,6 @@ if (count($_GET) == 0) {
     $iframe .= '.php';
   }
 }
-
-
-
-
-
-
-// Recursive function used to print the namespaces.
-function print_namespace($item, $name, $fullkey, $islast) {
-  global $config, $server, $redis;
-
-  // Is this also a key and not just a namespace?
-  if (isset($item['__phpredisadmin__'])) {
-    // Unset it so we won't loop over it when printing this namespace.
-    unset($item['__phpredisadmin__']);
-
-    $type  = $redis->type($fullkey);
-    $class = array();
-    $len   = false;
-
-    if (isset($_GET['key']) && ($fullkey == $_GET['key'])) {
-      $class[] = 'current';
-    }
-    if ($islast) {
-      $class[] = 'last';
-    }
-
-    // Get the number of items in the key.
-    if (!isset($config['faster']) || !$config['faster']) {
-      switch ($type) {
-        case 'hash':
-          $len = $redis->hLen($fullkey);
-          break;
-
-        case 'list':
-          $len = $redis->lLen($fullkey);
-          break;
-
-        case 'set':
-          // This is currently the only way to do this, this can be slow since we need to retrieve all keys
-          $len = count($redis->sMembers($fullkey));
-          break;
-
-        case 'zset':
-          // This is currently the only way to do this, this can be slow since we need to retrieve all keys
-          $len = count($redis->zRange($fullkey, 0, -1));
-          break;
-      }
-    }
-
-
-    ?>
-    <li<?php echo empty($class) ? '' : ' class="'.implode(' ', $class).'"'?>>
-    <a href="?view&amp;s=<?php echo $server['id']?>&amp;key=<?php echo urlencode($fullkey)?>"><?php echo format_html($name)?><?php if ($len !== false) { ?><span class="info">(<?php echo $len?>)</span><?php } ?></a>
-    </li>
-    <?php
-  }
-
-  // Does this namespace also contain subkeys?
-  if (count($item) > 0) {
-    ?>
-    <li class="folder<?php echo ($fullkey === '') ? '' : ' collapsed'?><?php echo $islast ? ' last' : ''?>">
-    <div class="icon"><?php echo format_html($name)?>&nbsp;<span class="info">(<?php echo count($item)?>)</span>
-    <?php if ($fullkey !== '') { ?><a href="delete.php?s=<?php echo $server['id']?>&amp;tree=<?php echo urlencode($fullkey)?>:" class="deltree"><img src="images/delete.png" width="10" height="10" title="Delete tree" alt="[X]"></a><?php } ?>
-    </div><ul>
-    <?php
-
-    $l = count($item);
-
-    foreach ($item as $childname => $childitem) {
-      // $fullkey will be empty on the first call.
-      if ($fullkey === '') {
-        $childfullkey = $childname;
-      } else {
-        $childfullkey = $fullkey.$server['seperator'].$childname;
-      }
-
-      print_namespace($childitem, $childname, $childfullkey, (--$l == 0));
-    }
-
-    ?>
-    </ul>
-    </li>
-    <?php
-  }
-}
-
 
 
 
@@ -165,6 +156,8 @@ require 'includes/header.inc.php';
 <?php } ?>
 </select>
 </p>
+
+<?php if($redis): ?>
 
 <p>
 <?php if (isset($login)) { ?>
@@ -191,6 +184,10 @@ require 'includes/header.inc.php';
 <?php print_namespace($namespaces, 'Keys', '', empty($namespaces))?>
 </ul>
 </div><!-- #keys -->
+
+<?php else: ?>
+<div style="color:red">Can't connect to this server</div>
+<?php endif; ?>
 
 <div id="frame">
 <iframe src="<?php echo format_html($iframe)?>" id="iframe" frameborder="0" scrolling="0"></iframe>
