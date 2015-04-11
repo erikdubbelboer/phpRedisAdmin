067db7ee9993bf7b0c109cfe29fc192673d66cd8
Erik Dubbelboer <erik@dubbelboer.com>
2015-04-11 07:15:32 +0000

    Fix rename, 1.2.1

--- rename.php
+++ rename.php
@@ -18,7 +18,7 @@ if (isset($_POST['old'], $_POST['key'])) {
 
   ?>
   <script>
-  top.location.href = top.location.pathname+'?view&s=<?php echo $server['id']?>&amp;d=<?php echo $server['db']?>&key=<?php echo urlencode($_POST['key'])?>';
+  top.location.href = top.location.pathname+'?view&s=<?php echo $server['id']?>&d=<?php echo $server['db']?>&key=<?php echo urlencode($_POST['key'])?>';
   </script>
   <?php
 
