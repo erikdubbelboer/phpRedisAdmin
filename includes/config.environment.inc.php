bfd4a28c2ddaa8ae5c2cb2cd445992054b0e209b
LeoHsiao <leohsiao@foxmail.com>
2024-06-20 17:42:45 +0800

    Get more configs params from environment variables

--- includes/config.environment.inc.php
+++ includes/config.environment.inc.php
@@ -2,6 +2,18 @@
 
 include 'config.sample.inc.php';
 
+// get configs from environment variables
+$config['cookie_auth']             = getenv('COOKIE_AUTH')                 ?: false;
+$config['count_elements_page']     = getenv('COUNT_ELEMENTS_PAGE')         ?: 100;
+$config['faster']                  = getenv('FASTER')                      ?: true;
+$config['filter']                  = getenv('FILTER')                      ?: '*';
+$config['hideEmptyDBs']            = getenv('HIDE_EMPTY_DBS')              ?: false;
+$config['keys']                    = getenv('KEYS')                        ?: false;
+$config['maxkeylen']               = getenv('MAX_KEY_LEN')                 ?: 100;
+$config['scansize']                = getenv('SCAN_SIZE')                   ?: 1000;
+$config['seperator']               = getenv('SEPERATOR')                   ?: ':';
+$config['showEmptyNamespaceAsKey'] = getenv('SHOW_EMPTY_NAMESPACE_AS_KEY') ?: false;
+
 $admin_user = getenv('ADMIN_USER');
 $admin_pass = getenv('ADMIN_PASS');
 
@@ -37,29 +49,30 @@ while (true) {
   if (empty($server_name)) {
     $server_name = $server_host;
   }
-  
+
   if (empty($server_auth)) {
     $server_auth = "";
-  } 
+  }
 
   if (empty($server_port)) {
     $server_port = 6379;
   }
 
   $config['servers'][] = array(
-      'name'   => $server_name,
-      'host'   => $server_host,
-      'port'   => $server_port,
-      'filter' => '*',
+      'name'     => $server_name,
+      'host'     => $server_host,
+      'port'     => $server_port,
+      'filter'   => $config['filter'],
+      'scansize' => $config['scansize'],
   );
-  
+
   if (!empty($server_auth)) {
     $config['servers'][$i-1]['auth'] = $server_auth;
-  } 
-  
+  }
+
   if (!empty($server_databases)) {
     $config['servers'][$i-1]['databases'] = $server_databases;
-  } 
+  }
 
   $i++;
 }
